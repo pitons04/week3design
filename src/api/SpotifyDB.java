26fcfdcda61026232c7500394d9e1d01894e5588
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-09-29 02:18:38 -0400

    Added a maven file and did some code changes to SpotifyDB.java

--- src/api/SpotifyDB.java
+++ src/api/SpotifyDB.java
@@ -1,6 +1,7 @@
 package api;
 
 import okhttp3.*;
+import org.json.JSONObject;
 //should I move this to SongOrganizer.java ?
 public class SpotifyDB {
     private static final String API_URL = "https://api.spotify.com/v1";
@@ -12,9 +13,15 @@ public class SpotifyDB {
 
     public String createPlaylist(String userid) {
         //creates a playlist under this userid
-        OkHttpClient client = new OkHttpClient().newBuilder().build();
-        Request request = new Request.Builder().url(String.format("https://api.spotify.com/v1/users/:%/playlists", userid))
+        MediaType mediaType = MediaType.parse("application/json");
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("A PlayList", false); // false means it's a private playlist.
+        RequestBody body = RequestBody.create(mediaType, requestBody.toString());
+        Request request = new Request.Builder()
+                .url(String.format("https://api.spotify.com/v1/users/:%/playlists", userid))
+                .method("POST", body)
                 .addHeader("Authorization", API_TOKEN)
+                .addHeader("Content-Type", "application/json")
                 .build();
         return null;
     }
