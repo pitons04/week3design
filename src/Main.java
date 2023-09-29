57d994c0ec5ca6656fc5a035753aca83a720252b
jayuj <jasminewei082@gmail.com>
2023-09-29 12:30:53 -0400

    updated SpotiftDB call

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+import api.SpotifyDB;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/api/SpotifyDB.java
+++ src/api/SpotifyDB.java
@@ -1,6 +1,11 @@
 package api;
 
 import okhttp3.*;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.io.IOException;
+
 //should I move this to SongOrganizer.java ?
 public class SpotifyDB {
     private static final String API_URL = "https://api.spotify.com/v1";
@@ -13,9 +18,22 @@ public class SpotifyDB {
     public String createPlaylist(String userid) {
         //creates a playlist under this userid
         OkHttpClient client = new OkHttpClient().newBuilder().build();
-        Request request = new Request.Builder().url(String.format("https://api.spotify.com/v1/users/:%/playlists", userid))
+        Request request = new Request.Builder().url(String.format("https://api.spotify.com/v1/users/:%s/playlists", userid))
                 .addHeader("Authorization", API_TOKEN)
                 .build();
-        return null;
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response);
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getInt("status_code") == 200) {
+                JSONObject playlist = responseBody.getJSONObject("playlist");
+                return playlist.getString("playlist");
+            } else {
+                throw new RuntimeException(responseBody.getString("message"));
+            }
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
